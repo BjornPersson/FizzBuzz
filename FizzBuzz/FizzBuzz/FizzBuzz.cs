45cf07472f428d847cee1fd89b374e30b047a110
Björn Persson <bjorn@cedur.se>
2013-07-05 14:42:05 +0200

    ok for 1 2 3 5 15

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public static string GetStringFor(int number)
+        {
+            if (number == 15)
+                return "FizzBuzz";
+
+            if (number % 3 == 0)
+                return "Fizz";
+
+            if (number % 5 == 0)
+                return "Buzz";
+
+            return number.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
--- FizzBuzz/FizzBuzz/UnitTest1.cs
+++ FizzBuzz/FizzBuzz/UnitTest1.cs
@@ -7,8 +7,33 @@ namespace FizzBuzz
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetStringFor1()
         {
+            Assert.AreEqual("1", FizzBuzz.GetStringFor(1));
+        }
+
+        [TestMethod]
+        public void GetStringFor2()
+        {
+            Assert.AreEqual("2", FizzBuzz.GetStringFor(2));
+        }
+
+        [TestMethod]
+        public void GetStringFor3()
+        {
+            Assert.AreEqual("Fizz", FizzBuzz.GetStringFor(3));
+        }
+
+        [TestMethod]
+        public void GetStringFor5()
+        {
+            Assert.AreEqual("Buzz", FizzBuzz.GetStringFor(5));
+        }
+
+        [TestMethod]
+        public void GetStringFor15()
+        {
+            Assert.AreEqual("FizzBuzz", FizzBuzz.GetStringFor(15));
         }
     }
 }
